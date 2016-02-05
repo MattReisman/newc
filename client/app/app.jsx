bb2779e2b8e6e97bf660a9c5cc9581ec66903713
MattReisman <mattreisman@gmail.com>
2016-02-05 13:57:51 -0500

    hide completed tasks

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -1,50 +1,69 @@
 App = React.createClass({
-  mixins: [ReactMeteorData],
+    mixins: [ReactMeteorData],
+    getInitialState() {
+        return {
+            hideCompleted: false
+        };
+    },
 
-  getMeteorData() {
-    return {
-      submissions: Submissions.find({}, {sort: {createdAt: -1}}).fetch(),
-    };
-  },
+    getMeteorData() {
+        let query = {};
 
-  renderSubmissions() {
-    return this.data.submissions.map((submission) => {
-      return <Submission key={submission._id} submission={submission} />;
-    });
-  },
+        if (this.state.hideCompleted) {
+            query = {checked: {$ne: true}};
+        }
+        return {
+            submissions: Submissions.find(query, {
+                sort: {
+                    createdAt: -1
+                }
+            }).fetch()
+        };
+    },
+    renderSubmissions() {
+        return this.data.submissions.map((submission) => {
+                return <Submission key={submission._id} submission={submission} />;
+            });
+    },
+    handleSubmit(event) {
+        event.preventDefault();
 
-  handleSubmit(event) {
-    event.preventDefault();
+        // Pull text from field
 
-    // Pull text from field
-    var text = React.findDOMNode(this.refs.textInput).value.trim();
-    Submissions.insert({
-      text: text,
-      createdAt: new Date(),
-    });
+        var text = React
+            .findDOMNode(this.refs.textInput)
+            .value
+            .trim();
+        Submissions.insert({text: text, createdAt: new Date()});
 
-    // Clear form
-    React.findDOMNode(this.refs.textInput).value = '';
-  },
+        // Clear form
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Submissions</h1>
-<form className='new-submission' onSubmit={this.handleSubmit} >
-          <input
-            type="text"
-            ref="textInput"
-            placeholder="Type to add new tasks" />
-            </form>
-        </header>
-
-        <ul>
-          {this.renderSubmissions()}
-        </ul>
-      </div>
-// <News />
-    );
-  },
+        React
+            .findDOMNode(this.refs.textInput)
+            .value = "";
+    },
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted
+        });
+    },
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Submissions</h1>
+                    <label className="hide-completed">
+                        <input type="checkbox" readOnly={true} checked={this.state.hideCompleted} onClick={this.toggleHideCompleted}/>
+                        Hide Completed Activities
+                    </label>
+                    <form className="new-submission" onSubmit={this.handleSubmit}>
+                        <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
+                    </form>
+                </header>
+                <ul>
+                    {this.renderSubmissions()}
+                </ul>
+            </div>
+        );
+    }
 });
