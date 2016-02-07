88957f849af848714f5f7650c68b2f341ddd99ef
MattReisman <mattreisman@gmail.com>
2016-02-07 04:12:03 -0500

    add packages

--- client/modules/submission/submission.jsx
+++ client/modules/submission/submission.jsx
@@ -26,22 +26,27 @@ Submission = React.createClass({
             .fromNow();
         // const momDate = "test';
 
-        return (
-            <li className={submissionClassName}>
-                <button className='delete' onClick={this.deleteThisSubmission}>
-                    &times;
-                </button>
-                <input type='checkbox' readOnly={true} checked={this.props.submission.checked} onClick={this.toggleChecked}/>
-                <span className='text'>
-                    {this.props.submission.text}
-                </span>
-                <span className='time-elapsed'>
-                    {elapsedTime}
-                </span>
-                <span className='subDate'>
-                    {formattedDate}
-                </span>
-            </li>
-        );
-    }
+        return (< li className = {
+            submissionClassName
+        } > < button className = 'delete' onClick = {
+            this.deleteThisSubmission
+        } > & times;
+        </button> < input type = 'checkbox'
+			readOnly = {
+				true
+			}
+			checked = {
+				this.props.submission.checked
+			}
+			onClick = {
+				this.toggleChecked
+			}
+			/ > < span className = 'text' > {
+            this.props.submission.text
+        } < /span> < span className = 'time-elapsed' > {
+				elapsedTime
+			} < /span > < span className = 'subDate' > {
+            formattedDate
+        } < /span> < /li >);
+    },
 });
